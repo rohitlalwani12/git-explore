03b911d61218da4c7ba0886ea6b916749be1d052
rohit.lalwani622 <rohit.lalwani622@gmail.com>
2026-03-13 00:32:30 +0530

    branch first commit

--- tests/openBrowser.spec.ts
+++ tests/openBrowser.spec.ts
@@ -3,3 +3,8 @@ import { test, expect } from '@playwright/test';
 test('open Browser',async({page})=>{
     await page.goto('https://www.youtube.com/')
 })
+
+test('open chrome', async ({ page }) => {
+    await page.goto('https://www.google.com/');
+    console.log('opening a browser ')
+})
