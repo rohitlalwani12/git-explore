944ceb9ab3d65976d99b147ad7482c4c0a77c6d2
rohit.lalwani622 <rohit.lalwani622@gmail.com>
2026-03-13 00:34:53 +0530

    branch second  commit

--- tests/openBrowser.spec.ts
+++ tests/openBrowser.spec.ts
@@ -8,3 +8,8 @@ test('open chrome', async ({ page }) => {
     await page.goto('https://www.google.com/');
     console.log('opening a browser ')
 })
+
+test('open firefox', async ({ page }) => {
+    await page.goto('https://www.gmail.com/');
+    console.log('opening a browser for gmail ')
+})
