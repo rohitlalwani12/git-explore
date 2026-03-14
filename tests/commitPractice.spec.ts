1f90eb9c0160d67ec374a10532b69ed47f83489a
rohit.lalwani622 <rohit.lalwani622@gmail.com>
2026-03-14 12:10:31 +0530

    Second Commit

--- tests/commitPractice.spec.ts
+++ tests/commitPractice.spec.ts
@@ -3,4 +3,6 @@ import test from "@playwright/test";
 test('first commit', async ({ page }) => {
     console.log('First Commit');
 })
-
+test('Second commit', async ({ page }) => {
+    console.log('Second Commit');
+})
